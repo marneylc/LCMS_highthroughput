96e75014276545ef1dd41b2beaae92ce322a2f3d
Luke Marney <Luke.Marney@mrc-hnr.cam.ac.uk>
2014-06-05 16:48:05 +0100

    code to produce plots of chromatograms

--- mrm.py
+++ mrm.py
@@ -15,26 +15,50 @@ import pymzml
 import os
 import pandas
 import matplotlib
+import webbrowser
 os.chdir('C:/github/LCMS_highthroughput/')
 from lcms import *
+from pylab import *
 
 os.chdir('C:/Users/marneyl/Downloads/testconvert/')
 files = pygrep('.mzML', '.')
 
-data = getMRM(files[2])
+filename = files[2]
+data = getSRM(filename)
 
-# plot a single transition
-plotMRM('225.2-44.2', data['transitions'])
-
-# plot all of them
-for cid in data['masses']:
-    plotMRM(cid, data['transitions'])
-    
-# make a table of transitions to look at if you want
-TT = pandas.DataFrame(transitions.keys(), index=numtransition, columns = {'Transitions'})
+''' 
+Below is how to show all SRM transitions and plot them.
+In the future, data['transitions'] could be restructured to only include the wanted transitions.
+First you make an html table and display it in a webrowser
+'''
+# make a table of transitions to look at
+TT = pandas.DataFrame(data['transitions'].keys(), index=data['numtransition'], columns = {'Transitions'})
 TT.to_html('transitions.html')
+url = "file://" + os.getcwd() + '\\transitions.html'
+webbrowser.open_new_tab(url)
 
-def getMRM(filename):
+# to plot a single transition
+fig1 = figure()
+plotSRM('758.6-184', data['transitions'])
+# use >>> show() to display figure if not in an IDE
+
+# plot all SRMs
+fig2 = figure()
+fig.suptitle(filename)
+for cid in data['masses']:
+    plotSRM(cid, data['transitions'])
+
+# set some parameters for the chromatogram
+ax=gca() # set axis object to ax
+ax.set_xticklabels(ax.get_xticks(), fontsize=20)
+sci_yticks = list()
+for t in ax.get_yticks():
+    sci_yticks.append("%.1E" % t)
+
+ax.set_yticklabels(sci_yticks, fontsize=20)
+show()
+
+def getSRM(filename):
     data = dict()
     transitions = dict()
     numtransition = list()
@@ -52,8 +76,8 @@ def getMRM(filename):
     data['masses'] = masses
     return data
 
-def plotMRM(cid, transitions):
-    plot(transitions[cid][0],transitions[cid][1])
+def plotSRM(cid, transitions):
+    plot(transitions[cid][0],transitions[cid][1],)
 
 
 
