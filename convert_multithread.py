3f3fdaa85852fe9f2917b535a062cf8f25291a3e
Luke Marney <marneylc@gmail.com>
2014-10-15 16:22:17 +0100

    made easier for windows

--- convert_multithread.py
+++ convert_multithread.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
-
 import os
 import re
 import shutil
 import threading
 import time
+import sys
 home = os.getcwd()
-os.chdir('/home/marneyl/LCMS_highthroughput/')
+os.chdir('C:/github/LCMS_highthroughput/')
 from lcms import *
 os.chdir(home)
-rawfiles = pygrep('mzXML','.')
-maxnumthreads = 4
+path = sys.argv[1] # path to folder of files to convert to mzXML
 
-for f in rawfiles:
-  t = mzXML_conv(f)
-  t.start()
-  while threading.activeCount() > maxnumthreads:
-    time.sleep(0)
+def main():
+	maxnumthreads = 4
+	os.chdir(path)
+	rawfiles = pygrep('raw','.')
+	for f in rawfiles:
+		t = mzXML_conv(f)
+		t.start()
+		while threading.activeCount() > maxnumthreads:
+			time.sleep(0)
 
+main()
